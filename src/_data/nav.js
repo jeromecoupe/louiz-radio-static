f6b7cee4f7fa37fb3dbcecd042f11dfc3f97ba28
Jérôme Coupé <jercoupe@gmail.com>
2022-04-22 16:25:06 +0200

    programs list

---
+++ src/_data/nav.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    label: "Accueil",
+    trigger: "home",
+    url: "/",
+  },
+  {
+    label: "Emissions",
+    trigger: "programs",
+    url: "/emissions/",
+  },
+  {
+    label: "A propos",
+    trigger: "about",
+    url: "/a-propos/",
+  },
+  {
+    label: "Contact",
+    trigger: "contact",
+    url: "/contact/",
+  },
+];
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "Louiz Radio",
-  url: "htpps://louizradio.netlify.app",
+  url: "https://louizradio.netlify.app",
   metaTitle: "Louiz Radio",
   metaDescription:
     "Louïz est une radio dont les programmes sont créés par et pour de jeunes adultes. Nous souhaitons pratiquer le slow journalisme, la qualité plutôt que la quantité, le temps long plutôt que les réactions à chaud et à l'actualité.",
